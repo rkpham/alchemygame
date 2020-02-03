9a847747530a3836f6fc55fecf58109d1f35373f
rkpham <30171670+rkpham@users.noreply.github.com>
2020-02-03 09:56:45 -0500

    random fixes

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,11 +3,12 @@ extends Node
 func _ready():
 	# Set appropriate game resolution
 	var screen_size = OS.get_screen_size()
-	if int(fmod(screen_size.x, 640)) == fmod(screen_size.x, 640):
+	print(screen_size)
+	if int(fmod(screen_size.x, 640)) == 0:
 		print("Screen is 16:9")
 		var screen_size_set = Vector2(640, 360)
-		OS.window_size = screen_size_set
-	elif int(fmod(screen_size.x, 683)) == fmod(screen_size.x, 683):
+		OS.set_window_size(screen_size_set)
+	elif int(fmod(screen_size.x, 683)) == 0:
 		print("Screen is 1366x768")
 		var screen_size_set = Vector2(683, 384)
-		OS.window_size = screen_size_set
+		OS.set_window_size(screen_size_set)
