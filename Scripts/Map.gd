6f83b85fa2360e17f3a6ae18859df883e9f6dbf0
rkpham <trumpkp@gmail.com>
2020-02-12 22:27:30 -0500

    readying for random maps

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,13 +1,13 @@
 extends TileMap
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var enemies = []
 
-# Called when the node enters the scene tree for the first time.
+var rooms = {
+	"grass1": "res://Rooms/Grass1.tscn"
+}
+
+func change_map(map_name):
+	pass
+
 func _ready():
 	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
