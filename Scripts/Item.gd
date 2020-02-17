0d9a0cfad02d5856035eb11d2e361c0b4a592d38
rkpham <30171670+rkpham@users.noreply.github.com>
2020-02-17 16:51:29 -0500

    Tile changes

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -9,7 +9,7 @@ var float_speed = 2
 var float_amp = 10
 
 func _process(delta):
-	material.set_shader_param("outLineSize" , int(outline)*0.02)
+	material.set_shader_param("outLineSize" , int(outline)*0.005)
 	float_cycle += delta * float_speed
 	float_cycle = fmod(float_cycle, float_cycle_end)
 	offset = Vector2(0, sin(float_cycle)*float_amp-float_amp)
